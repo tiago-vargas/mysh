e48999c098a6cf8ca8dbe71d728af42dcc97ad77
Tiago Vargas <tiago.ce10@outlook.com>
2023-02-22 18:52:25 -0300

    add unit testing module

---
+++ lib/unit_tests.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+#define RED         "\e[31m"
+#define GREEN       "\e[32m"
+#define RESET_COLOR "\e[0m"
+
+void run_test(bool (*test_case)(), char *description)
+{
+	if (test_case())
+		printf(GREEN "PASSED" RESET_COLOR ": %s" "\n", description);
+	else
+		printf(RED   "FAILED" RESET_COLOR ": %s" "\n", description);
+}
