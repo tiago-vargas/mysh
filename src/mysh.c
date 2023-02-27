0733d14578f50e3719ab44abfc4dd077ea2555e4
Tiago Vargas <tiago.ce10@outlook.com>
2023-02-27 20:47:00 -0300

    execute single command optionally with arguments

--- src/mysh.c
+++ src/mysh.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-int main()
+void parse_arguments(/*out*/ char *arguments[], char **argv, int argc)
 {
-	printf("lib  out  src" "\n");
+	int last_index = argc - 1;
+	for (int i = 0; i < last_index; i++)
+		arguments[i] = argv[i + 1];
+
+	arguments[last_index] = NULL;
+}
+
+int main(const int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		/// Usage: `mysh command [options]`
+
+		char *executable_path = argv[1];
+		char *arguments[argc];
+		parse_arguments(/*out*/ arguments, argv, argc);
+
+		execv(executable_path, arguments);
+	}
 
 	return 0;
 }
